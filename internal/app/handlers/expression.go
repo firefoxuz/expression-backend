4aa9a769bd899df4177a62f10341d19c2d2141d2
Sultonov Daler <daler.sultonov.00@gmail.com>
2024-02-07 22:53:54 +0500

    -

--- internal/app/handlers/expression.go
+++ internal/app/handlers/expression.go
@@ -1,12 +1,38 @@
 package handlers
 
 import (
+	"encoding/json"
+	"expression-backend/internal/app/services"
+	"fmt"
+	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"net/http"
 )
 
+type Expression struct {
+	Exp string `json:"expression" validate:"required"`
+}
+
 func StoreExpression(writer http.ResponseWriter, request *http.Request) {
-	writer.Write([]byte("method post"))
+	body, err := ioutil.ReadAll(request.Body)
+
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		writer.Write([]byte("something went wrong"))
+	}
+
+	exp := Expression{}
+
+	json.Unmarshal(body, &exp)
+
+	validate := validator.New(validator.WithRequiredStructEnabled())
+
+	err = validate.Struct(exp)
+
+	fmt.Println(err)
+	fmt.Println(exp.Exp)
+	fmt.Println(services.IsValidMathExpression(exp.Exp))
 }
 
 func GetExpressions(writer http.ResponseWriter, request *http.Request) {
